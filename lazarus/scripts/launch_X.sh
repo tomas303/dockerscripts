bee3ccc6835b80edb807d787942a09301fefb427
tomas303 <tomashorak@post.cz>
2020-11-15 20:37:08 +0100

    modify script for starting xwindows to aply resolution via xrandr

--- lazarus/scripts/launch_X.sh
+++ lazarus/scripts/launch_X.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#ENV TZ=America/Los_Angeles
+#if [ -z "$TZ" ]
+#then
+#  ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+#fi
+
 /lib/systemd/systemd-udevd &
 udevadm trigger
 
