a92ee43ce0abdd00f3bec170883fd753e7f0b7bc
tomas <tomas@localhost.localdomain>
2026-01-24 15:32:21 +0100

    add task extension

--- go/setup.sh
+++ go/setup.sh
@@ -5,15 +5,13 @@
 ################################################################################
 
 . $ScriptsDir/funcs.sh
+. $ScriptsDir/setup_default.sh
 
 install_extension "aaron-bond.better-comments"
 install_extension "CoenraadS.bracket-pair-colorizer"
 install_extension "golang.go"
 install_extension "mike-lischke.vscode-antlr4"
 install_extension "tamasfe.even-better-toml"
-install_extension "GitHub.copilot"
-install_extension "GitHub.copilot-chat"
-install_extension "alefragnani.numbered-bookmarks"
 install_extension "qwtel.sqlite-viewer"
 
 /usr/local/go/bin/go install -v github.com/go-delve/delve/cmd/dlv@latest
--- lib/setup_default.sh
+++ lib/setup_default.sh
@@ -3,4 +3,5 @@
 install_extension "GitHub.copilot"
 install_extension "GitHub.copilot-chat"
 install_extension "alefragnani.numbered-bookmarks"
+install_extension "task.vscode-task"
 
